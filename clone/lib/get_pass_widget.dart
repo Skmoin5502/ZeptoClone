9c5a5a0ed8975daa08780ccd09f9cdbbdf3f7dbb
SK Moinuddin <98759447+Skmoin5502@users.noreply.github.com>
2024-05-08 19:42:13 +0530

    get_pass_widget.dart

--- clone/lib/get_pass_widget.dart
+++ clone/lib/get_pass_widget.dart
@@ -4,11 +4,11 @@ class GetPassWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(8), // Adjust the border radius as needed
+      borderRadius: BorderRadius.circular(8), 
       child: Image.asset(
-        'assets/images/getpass.jpg', // Your image path
-        width: 60, // Adjust width as needed
-        height: 44, // Adjust height as needed
+        'assets/images/getpass.jpg', 
+        width: 60, 
+        height: 44,
         fit: BoxFit.cover,
       ),
     );
